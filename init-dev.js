71502a705b864afd43a3d3d334824be5e5a5ebcb
Kan <safghjkl@gmail.com>
2019-03-11 11:42:04 +0800

    Update init-dev.js

--- init-dev.js
+++ init-dev.js
@@ -1,11 +1,11 @@
 db = db.getSiblingDB('service-rss-reader-');
 db.createUser({
-    user: "service-rss-reader-",
+    user: "service-rss-reader",
     pwd: "pass",
     roles: [
         {
             role: "readWrite",
-            db: "service-rss-reader-"
+            db: "service-rss-reader-dev"
         }
     ]
   });
